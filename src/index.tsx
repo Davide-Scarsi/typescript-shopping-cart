fa8e24c241767f91f2d881daad5facf8e67d8b28
Davide Scarsi <davide@cavaliere.io>
2023-08-29 10:19:27 +0200

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter(
+  [
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "typescript-shopping-cart",
+    element: <App />,
+  }
+]);
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+  return (
+    <>
+    <div className="vh-100 bg-dark text-white">
+      <h1>ciao</h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
